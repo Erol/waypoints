53319434dfd09526970bd7fe725cade259fe6dab
Erol <erol.fornoles@gmail.com>
2018-07-05 17:44:30 +0800

    Use constant

--- lib/waypoints/engine.rb
+++ lib/waypoints/engine.rb
@@ -3,7 +3,7 @@ require "waypoints/middleware"
 module Waypoints
   class Engine < ::Rails::Engine
     initializer "waypoints.middleware" do |app|
-      app.config.app_middleware.use "Waypoints::Middleware"
+      app.config.app_middleware.use Waypoints::Middleware
     end
   end
 end
